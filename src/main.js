de0a7c2c398ce3c92b77778e6adecb42758c9a4d
Andrew <bitrixzzakazz@gmail.com>
2025-02-03 15:54:15 +0700

    Обновлен main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from "./router";
 import vuetify from "./plugins/vuetify"; // ✅ Подключаем Vuetify
 
 const app = createApp(App);
+console.log(
+  "Telegram WebApp:",
+  window.Telegram ? window.Telegram.WebApp : "Не найден"
+);
 
 app.use(router); // ✅ Подключаем маршрутизацию
 app.use(vuetify); // ✅ Подключаем Vuetify
